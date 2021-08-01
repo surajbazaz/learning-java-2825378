7b354d64b83269e3a5cffc13d22a53d8dc66add1
suraj bazaz - intelliJ <surajbazaz.2009@gmail.com>
2021-08-01 16:07:49 +0530

    first change for systemout

---
+++ source/src/examplesTry/Main.java
@@ -0,0 +1,15 @@
+package examplesTry;
+
+class Main {
+    public static void main(String[] args) {
+        int roll = 1;
+        char grade = 'A';
+        double GPA = 4.5;
+        String name = "Anand";
+
+        System.out.println("Roll number : "+roll+" is having grade " +grade);
+        System.out.println(GPA);
+        System.out.println("Name : "+name);
+
+    }
+}
