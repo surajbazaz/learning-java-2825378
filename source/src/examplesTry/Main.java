fa8d85422e13a7530039f035d4e33eaf710800fe
suraj bazaz - intelliJ <surajbazaz.2009@gmail.com>
2021-08-01 16:28:42 +0530

    changes for student initials

--- source/src/examplesTry/Main.java
+++ source/src/examplesTry/Main.java
@@ -5,11 +5,20 @@ class Main {
         int roll = 1;
         char grade = 'A';
         double GPA = 4.5;
-        String name = "Anand";
+        String fName = "Anand";
+        String lName = "Sharma";
 
         System.out.println("Roll number : "+roll+" is having grade " +grade);
         System.out.println(GPA);
-        System.out.println("Name : "+name);
+        System.out.println("First Name : "+fName);
+        System.out.println("Last Name : "+lName);
+
+        char initialFirst = fName.charAt(0);
+        char initialSecond = lName.charAt(0);
+        System.out.println("Students initials are " +initialFirst+"."+initialSecond+".");
+
+
+
 
     }
 }
